d9d8a1891232659377e64330fde9b5c96b8201aa
Matheus Picoli <matheuspicoli2011@gmail.com>
2024-06-07 18:44:58 -0300

    collections change according to the season

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -11,6 +11,40 @@ interface SectionProps {
 	reference: "masculine" | "feminine" | "teens" | "collections"
 }
 
+type Season = "summer" | "autumn" | "winter" | "spring"
+
+function isDateInRange(
+	day: number,
+	month: number,
+	startMonth: number,
+	startDay: number,
+	endMonth: number,
+	endDay: number
+): boolean {
+	const startDate = new Date(new Date().getFullYear(), startMonth - 1, startDay)
+	const endDate = new Date(new Date().getFullYear(), endMonth - 1, endDay)
+	const currentDate = new Date(new Date().getFullYear(), month - 1, day)
+
+	return currentDate >= startDate && currentDate < endDate
+}
+
+function getSeason(date: Date = new Date()): Season {
+	const day = date.getDate()
+	const month = date.getMonth() + 1
+
+	if (isDateInRange(day, month, 12, 22, 3, 20)) {
+		return "summer"
+	} else if (isDateInRange(day, month, 3, 20, 6, 21)) {
+		return "autumn"
+	} else if (isDateInRange(day, month, 6, 21, 9, 23)) {
+		return "winter"
+	} else if (isDateInRange(day, month, 9, 23, 12, 22)) {
+		return "spring"
+	}
+
+	throw new Error("Data inválida")
+}
+
 export default function Section<T extends SectionProps>(props: T): React.JSX.Element {
 	return (
 		<React.Fragment>
@@ -54,7 +88,7 @@ export default function Section<T extends SectionProps>(props: T): React.JSX.Ele
 								key={index}
 								className="teens-sneaker-photo"
 								src={sneaker}
-								alt={`Tênis masculino ${index + 1}`}
+								alt={`Tênis adolescente ${index + 1}`}
 								loading="lazy"
 								priority={false}
 							/>
@@ -64,6 +98,62 @@ export default function Section<T extends SectionProps>(props: T): React.JSX.Ele
 			)}
 			{props.reference === "collections" && (
 				<section id="collections" className="collections">
+					{getSeason() === "summer" && (
+						<div className="collection-sneakers">
+							{collectionSneakers.summer.map((sneaker: StaticImageData, index: number) => (
+								<Image
+									key={index}
+									className="collection-sneaker-photo"
+									src={sneaker}
+									alt={`Tênis verão ${index + 1}`}
+									loading="lazy"
+									priority={false}
+								/>
+							))}
+						</div>
+					)}
+					{getSeason() === "autumn" && (
+						<div className="collection-sneakers">
+							{collectionSneakers.autumn.map((sneaker: StaticImageData, index: number) => (
+								<Image
+									key={index}
+									className="collection-sneaker-photo"
+									src={sneaker}
+									alt={`Tênis outono ${index + 1}`}
+									loading="lazy"
+									priority={false}
+								/>
+							))}
+						</div>
+					)}
+					{getSeason() === "winter" && (
+						<div className="collection-sneakers">
+							{collectionSneakers.winter.map((sneaker: StaticImageData, index: number) => (
+								<Image
+									key={index}
+									className="collection-sneaker-photo"
+									src={sneaker}
+									alt={`Tênis inverno ${index + 1}`}
+									loading="lazy"
+									priority={false}
+								/>
+							))}
+						</div>
+					)}
+					{getSeason() === "spring" && (
+						<div className="collection-sneakers">
+							{collectionSneakers.spring.map((sneaker: StaticImageData, index: number) => (
+								<Image
+									key={index}
+									className="collection-sneaker-photo"
+									src={sneaker}
+									alt={`Tênis primavera ${index + 1}`}
+									loading="lazy"
+									priority={false}
+								/>
+							))}
+						</div>
+					)}
 				</section>
 			)}
 		</React.Fragment>
