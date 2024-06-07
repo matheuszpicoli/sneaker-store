ecfa4d4118f7d302ec2c18a4470a69603c2de1ed
Matheus Picoli <matheuspicoli2011@gmail.com>
2024-06-07 14:38:52 -0300

    changing the logo and adjusting the z-index of the header

--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -1,3 +1,3 @@
 //- React icons
-export { GiConverseShoe as Sneaker } from "react-icons/gi"
+export { GiRunningShoe as Sneaker } from "react-icons/gi"
 export { GrFormSearch as MagnifyingGlass } from "react-icons/gr"
