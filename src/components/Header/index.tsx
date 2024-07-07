fd43db495e319aa3877f62f83d265301dde81555
Matheus Picoli <matheuspicoli2011@gmail.com>
2024-07-07 10:52:41 -0300

    removing the favorite screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,6 @@ import * as Icon from "@/icons/icons"
 
 //- Components
 import CartScreen from "../CartScreen"
-import FavoriteScreen from "../FavoriteScreen"
 import FilterSceen from "../FilterScreen"
 
 import Logo from "./Logo"
@@ -20,22 +19,20 @@ interface HeaderProps {
 }
 
 export default function Header(props: HeaderProps): React.JSX.Element {
-	const [favoriteScreen, setFavoriteScreen] = useState<boolean>(false)
 	const [filterScreen, setFilterScreen] = useState<boolean>(false)
 	const [cartScreen, setCartScreen] = useState<boolean>(false)
 
 	useLayoutEffect(() => {
-		if (favoriteScreen || filterScreen || cartScreen) document.body.classList.add("no-events")
+		if (filterScreen || cartScreen) document.body.classList.add("no-events")
 		else document.body.classList.remove("no-events")
-	}, [favoriteScreen, filterScreen, cartScreen])
+	}, [filterScreen, cartScreen])
 
 	useLayoutEffect(() => {
 		const handleKeyDown = (event: KeyboardEvent): void => {
 			const esc: string = "Escape"
 
 			if (event.key === esc) {
-				if (favoriteScreen) setFavoriteScreen(false)
-				else if (filterScreen) setFilterScreen(false)
+				if (filterScreen) setFilterScreen(false)
 				else if (cartScreen) setCartScreen(false)
 			}
 		}
@@ -43,7 +40,7 @@ export default function Header(props: HeaderProps): React.JSX.Element {
 		window.addEventListener("keydown", handleKeyDown)
 
 		return () => window.removeEventListener("keydown", handleKeyDown)
-	}, [favoriteScreen, filterScreen, cartScreen, setFavoriteScreen, setFilterScreen, setCartScreen])
+	}, [filterScreen, cartScreen, setFilterScreen, setCartScreen])
 
 	return (
 		<header>
@@ -56,21 +53,6 @@ export default function Header(props: HeaderProps): React.JSX.Element {
 					<NavigateMenu navigateToSection="collections" linkText="Coleções" />
 				</div>
 				<div className="icons-area">
-					{favoriteScreen ? (
-						<section className="favorite" onClick={(): void => setFavoriteScreen(false)}>
-							<div className="icon-area">
-								<Icon.Favorite className="favorite-icon" title="Favoritos" />
-							</div>
-
-							<FavoriteScreen />
-						</section>
-					) : (
-						<section className="favorite" onClick={(): void => setFavoriteScreen(true)}>
-							<div className="icon-area">
-								<Icon.Favorite className="favorite-icon" title="Favoritos" />
-							</div>
-						</section>
-					)}
 					{filterScreen ? (
 						<section className="filter">
 							<div className="icon-area" onClick={(): void => setFilterScreen(false)}>
